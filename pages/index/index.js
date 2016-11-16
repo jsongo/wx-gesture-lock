9104fce08466ffd52dcaee29341c1dc657c11360
jason <jsongo@qq.com>
2016-11-16 15:55:16 +0800

    update style

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,12 @@ Page({
         title: '绘制解锁图案',
     },
     onLoad: function () {
-        app.lock = new Lock(this);
+        this.lock = new Lock(this);
     },
+    resetPwd: function() {
+        this.lock.updatePassword();
+    },
+    
     onTitleChanged: function(newTitle) { // 文字变化的事件，自定义
         this.setData({
             title: newTitle
